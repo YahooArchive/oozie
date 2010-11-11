c3df1a559821d0fc181017b08d31ae582c2d6153
Mayank Bansal <bansalm@wlanvpn-snve-246-59.corp.yahoo.com>
2010-11-10 19:30:29 -0800

    Closes GH-0098 Changing name VerifyXCommandException to PreconditionException

--- core/src/main/java/org/apache/oozie/command/PreconditionException.java
+++ core/src/main/java/org/apache/oozie/command/PreconditionException.java
@@ -3,13 +3,13 @@ package org.apache.oozie.command;
 import org.apache.oozie.ErrorCode;
 import org.apache.oozie.XException;
 
-public class VerifyXCommandException extends XException{
+public class PreconditionException extends XException{
     /**
      * Create a verifyXCommand exception for verify conditions purposes.
      *
      * @param cause the XException cause.
      */
-    public VerifyXCommandException(XException cause) {
+    public PreconditionException(XException cause) {
         super(cause);
     }
 
@@ -19,7 +19,7 @@ public class VerifyXCommandException extends XException{
      * @param errorCode error code.
      * @param params parameters for the error code message template.
      */
-    public VerifyXCommandException(ErrorCode errorCode, Object... params) {
+    public PreconditionException(ErrorCode errorCode, Object... params) {
         super(errorCode, params);
     }
 }
