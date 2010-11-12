0c40582a07b14d8a297a2af4cd55f5b6ffd0a30b
angelokh <angelokh@ymail.com>
2010-11-11 20:30:04 -0800

    Closes GH-0144 Add WorkflowActionUpdateCommand, WorkflowActionsGetForJobCommand, WorkflowIdGetForExternalIdCommand, WorkflowJobUpdateCommand

---
+++ core/src/main/java/org/apache/oozie/command/jpa/WorkflowIdGetForExternalIdCommand.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2010 Yahoo! Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. See accompanying LICENSE file.
+ */
+package org.apache.oozie.command.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.apache.oozie.command.CommandException;
+import org.apache.oozie.util.ParamChecker;
+
+/**
+ * Get the Workflow ID with given external ID which will be assigned for the subworkflows.
+ */
+public class WorkflowIdGetForExternalIdCommand implements JPACommand<String> {
+
+    private String externalId = null;
+
+    public WorkflowIdGetForExternalIdCommand(String externalId) {
+        ParamChecker.notNull(externalId, "externalId");
+        this.externalId = externalId;
+    }
+
+    @Override
+    public String getName() {
+        return "WorkflowIdGetForExternalIdCommand";
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public String execute(EntityManager em) throws CommandException {
+        String id = "";
+        Query q = em.createNamedQuery("GET_WORKFLOW_ID_FOR_EXTERNAL_ID");
+        q.setParameter("externalId", externalId);
+        List<String> w = q.getResultList();
+        if (w.size() == 0) {
+            id = "";
+        }
+        else {
+            int index = w.size() - 1;
+            id = w.get(index);
+        }
+        return id;
+    }
+}
