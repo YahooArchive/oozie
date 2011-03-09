48e4ad16c3261a54698299ecbe7a3d1a3ad705ce
Angelo Kaichen Huang <angelokh@ymail.com>
2011-03-09 14:33:15 -0800

    Closes GH-0578 Incorporate feedbacks

--- builds/runHudsonCIBuild.sh
+++ builds/runHudsonCIBuild.sh
@@ -12,10 +12,12 @@ export JAVA_HOME=${TOOLS_HOME}/java/latest
 export M3_HOME=${TOOLS_HOME}/maven/apache-maven-3.0.3
 export PATH=$JAVA_HOME/bin:$M3_HOME/bin:$PATH
 
-#Please uncomment this line if the git repo needs to download
+#Please uncomment these lines if the git repo needs to download
 #git clone -o origin $OOZIE_GIT_REPO $WORKSPACE
+#cd $WORKSPACE
 
-bin/mkdistro.sh $1 -Dmaven.repo.local=/tmp/m2$$
+M2DIR=`mktemp -d /tmp/oozie-m2.XXXXX`
+bin/mkdistro.sh $1 -Dmaven.repo.local=$M2DIR
 EXIT=$?
-rm -fr /tmp/m2$$
+rm -rf $M2DIR
 exit $EXIT
